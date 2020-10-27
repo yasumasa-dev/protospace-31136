7118867e0c3f46358ac828f4e80adf89d8ad05b0
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-27 20:20:15 +0900

    syntaxerrorの修正１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters #メソッド名は慣習
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name :profile :occupation :position])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
