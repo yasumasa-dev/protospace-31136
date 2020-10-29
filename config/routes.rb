6691b9b822c7f5158f2ab76a42fa9b15b4a600b9
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-30 02:08:47 +0900

    その他

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   get 'prototypes/index'
   root to:'prototypes#index'
   resources :users
-  resources :prototypes, only: [:new, :create] #do
-    #resources :name, only: [:index, :creste]
-  #end
+  resources :prototypes
 end
