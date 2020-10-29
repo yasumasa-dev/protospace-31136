4c23ff254c46ae09c5df6b1d347f407c9e0bed45
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-29 19:15:03 +0900

    モデルの誤字修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,6 +3,6 @@ class Prototype < ApplicationRecord
   has_one_attached :image
   validates :image, presence: true
   validates :title, presence: true
-  validates :catsh_copy, presence: true
+  validates :catch_copy, presence: true
   validates :concept, presence: true
 end
