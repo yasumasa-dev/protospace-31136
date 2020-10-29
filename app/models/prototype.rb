1caa162ad7da330603e97dff0612d714256a194a
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-29 15:37:55 +0900

    Prototypeモデルにバリデーションを設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,8 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :catsh_copy, presence: true
+  validates :concept, presence: true
 end
