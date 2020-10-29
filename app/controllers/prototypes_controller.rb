190ef10e3fcc5e99594ec7304064531396715228
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-29 19:14:20 +0900

    createアクションの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,26 @@
 class PrototypesController < ApplicationController
   def index
   end
+
   def destroy
   end
+
   def new
     @prototype = Prototype.new
   end
+
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :image).merge(user_id: current_user.id)
   end
 end
