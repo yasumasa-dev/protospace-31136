f33cd013cb72038df67fd8a2d55aba527e681377
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-30 02:19:35 +0900

    Revert "情報投稿機能"

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,51 +1,6 @@
 class PrototypesController < ApplicationController
   def index
-    @prototype = Prototype.all
   end
-
   def destroy
-    prototype = Prototype.find(params[:id])
-    if prototype.destroy
-      redirect_to root_path
-      else
-      render :show
-      end
-  end
-
-  def new
-    @prototype = Prototype.new
-  end
-
-  def create
-    @prototype = Prototype.create(prototype_params)
-    if @prototype.save #入力フォームデータベースにprototypeを保存
-      redirect_to root_path #保存できればルートパスに移動
-    else
-      render :new #入力フォームの値が空であれば新規投稿画面に移動
-    end
-  end
-
-  def show #showアクションにインスタンス変数@prototype定義
-    @prototype = Prototype.includes(:user).find(params[:id])
-    #@prototype = Prototype.find(params[:id])#pathパラメータで送信されるID値で特定のオブジェクトを取得し@prototypeに代入
-  end
-
-  def edit
-    @prototype = Prototype.find(params[:id])
-  end
-
-  def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)  
-      redirect_to prototype_path  
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def prototype_params #createアクションのバリデーション設定だっけ？
-    params.require(:prototype).permit(:title, :catch_copy, :concept,:image).merge(user_id: current_user.id)
   end
 end
