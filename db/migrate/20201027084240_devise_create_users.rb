16a03da920a7e7a1faf5e817aad44e0a83a642bd
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-10-29 12:35:29 +0900

    エラーの訂正

--- db/migrate/20201027084240_devise_create_users.rb
+++ db/migrate/20201027084240_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :neme,               null: false
+      t.string :name,               null: false
       t.text :profile,              null: false
       t.text :occupation,           null: false
       t.text :position,             null: false
