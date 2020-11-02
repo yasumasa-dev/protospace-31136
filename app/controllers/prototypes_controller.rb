b973f9cd7a11c1a8f561cde38ab7a595483b7eb5
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-02 10:26:55 +0900

    リダイレクト処理

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
+
 
   def index
     @prototype = Prototype.all
@@ -51,5 +53,11 @@ class PrototypesController < ApplicationController
 
   def prototype_params #createアクションのストロングパラメーターの設定
     params.require(:prototype).permit(:title, :catch_copy, :concept,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
